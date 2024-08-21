b90db3e9c235b3fcc1709911387920e76ab4a5ad
emanuelsantoz <emanuelsantossouzajesus@gmail.com>
2024-08-21 06:39:51 -0300

    Update | Colocando validação de campus

--- Modulo 2 - Arrays/desafios/scrabble.c
+++ Modulo 2 - Arrays/desafios/scrabble.c
@@ -15,7 +15,8 @@ int main(void)
     // Solicitar ao usuário duas palavras
     // Calcular os pontos de cada palavra
     int userOnePointsTotal = compute_score(get_string("Por favor digite uma palavra, player 1: "));
-    int userTwoPointsTotal = compute_score(get_string("\nPor favor digite uma palavra, player 2: "));
+    int userTwoPointsTotal =
+        compute_score(get_string("\nPor favor digite uma palavra, player 2: "));
 
     // Imprimir o vencedor
     if (userOnePointsTotal < userTwoPointsTotal)
@@ -35,11 +36,17 @@ int compute_score(string word)
     // Computa e devolve os pontos de cada palavra
     for (int i = 0, tamanhoTotal = strlen(word); i < tamanhoTotal; i++)
     {
-        int transformaWordToNumber = toupper(word[i]);
-        int indexWordNumber = transformaWordToNumber - 64;
-        pontoLetter = POINTS[indexWordNumber - 1];
+        // test the type
+        if (!isdigit(word[i]))
+        {
+            int transformaWordToNumber = toupper(word[i]);
+            int indexWordNumber = transformaWordToNumber - 64;
+            pontoLetter = POINTS[indexWordNumber - 1];
 
-        pontoLetterTotal += pontoLetter;
+            pontoLetterTotal += pontoLetter;
+        }else {
+            printf("This is not a word!\n");
+        }
     }
     printf("Pontoação final: %i\n", pontoLetterTotal);
     return pontoLetterTotal;
