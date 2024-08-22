d4b46abc0fc86b66216ba4f03e6acd90ecd9ad77
emanuelsantoz <emanuelsantossouzajesus@gmail.com>
2024-08-22 09:12:53 -0300

    Arrays | Readability | Inicio

---
+++ Modulo 2 - Arrays/desafios/readability.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void)
+{
+    // Input | pedir um texto para o usuario
+    string text = get_string("Text: ");
+    int totalLetter ;
+    int totalPalavra ;
+    int totalFrase;
+
+    // Calculo | Numero medio a cada 100 por letra L
+
+
+    // Calculo | numero medio a cada 100 sentaça S
+
+
+    // Calculo | Formula Coleman-Liau
+
+    // Saída | Resultado da fomra
+    printf("Grand ", );
+}
+
+int totalThings(string word){
+
+}
